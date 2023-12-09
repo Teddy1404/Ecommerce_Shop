a6a01519cfe47dc48c7fb858055c189eafd3f654
Teddy1404 <aryansinghchandel1404@gmail.com>
2023-12-09 14:48:58 +0530

    updated

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { AuthProvider } from "./context/auth";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <AuthProvider>
     <BrowserRouter>
