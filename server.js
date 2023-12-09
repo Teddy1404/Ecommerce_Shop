8f29d26b176ee5b5f6e4c2ff59f6f890857e0095
Aryan singh chandel <96349795+Teddy1404@users.noreply.github.com>
2023-12-09 15:32:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ import connectDB from "./config/db.js";
 import morgan from "morgan";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
-<<<<<<< HEAD
+
 import path from "path";
-=======
-import path from 'path'
+
 import {fileURLToPath} from 'url';
->>>>>>> 0ca6cf0b04875d83606a27971a2692578d337036
+
 const app = express();
 
 //env
@@ -27,12 +26,8 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 //deploy
-<<<<<<< HEAD
-// app.use(express.static(path.join(__dirname, "./client/build")));
-
-=======
 app.use(express.static(path.join(__dirname,'./client/build')))
->>>>>>> 0ca6cf0b04875d83606a27971a2692578d337036
+
 //routes
 app.use("/api/v1/auth", authRoutes);
 
