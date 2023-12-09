7611260311f09f7b5d7497ea25d503f7ea56a717
Aryan singh chandel <96349795+Teddy1404@users.noreply.github.com>
2023-12-09 14:51:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import morgan from "morgan";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
+import path from 'path'
 const app = express();
 
 //env
@@ -17,14 +18,20 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
+//deploy
+app.use(express.static(path.join(__dirname,'./client/build')))
 //routes
 app.use("/api/v1/auth", authRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>Welcome to ecommerce app</h1>");
+// });
+
+//deploy 
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname , "./client/build/index.html"))
+})
 
 //port
 const PORT = process.env.PORT || 8080;
